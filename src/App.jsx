fb827fb051207049ac6a78fdf5635aaef330ac47
maivietha1203 <vhdzvcl@gmail.com>
2026-07-06 10:28:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Feature from "./component/feature";
+
+function App() {
+  return (
+    <div>
+      <Feature></Feature>
+      <Feature></Feature>
+      <Feature></Feature>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/feature.jsx
@@ -0,0 +1,19 @@
+const feature = () => {
+  return (
+    <div>
+      <div className="feature">
+        <img src="" alt="" className="feature-image"></img>
+        <h3 className="feature-title">Title</h3>
+        <p className="feature-desc">
+          Lorem ipsum is a dummy or placeholder text commonly used in graphic
+          design, publishing, and web development. It is typically a corrupted
+          version of De finibus bonorum et malorum, a 1st-century BC text by the
+          Roman statesman and philosopher Cicero, with words altered, added, and
+          removed to make it nonsensical and improp...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default feature;
