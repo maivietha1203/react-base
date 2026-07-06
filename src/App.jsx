698cfb9fe0b2f75e383e552b18a52847fc5b0664
maivietha1203 <vhdzvcl@gmail.com>
2026-07-06 15:17:49 +0700

    Bài 14 - Rendering list phần 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import { YoutubeData } from "./YoutubeData";
 // import Feature from "./component/feature";
 
 function App() {
-  const numbers = [1, 2, 3, 4, 5];
-  const double = numbers.map((item) => item * 2);
-  console.log(double);
+  // const numbers = [1, 2, 3, 4, 5];
+  // const double = numbers.map((item) => item * 2);
+  // console.log(double);
   return (
     <div className="container">
+      {YoutubeData.map((item) => (
+        <YoutubeItem
+          key={item.id}
+          author={item.author}
+          title={item.title}
+          avatar={item.avatar}
+          image={item.image}
+        ></YoutubeItem>
+      ))}
       {/* <Feature></Feature>
       <Feature></Feature>
       <Feature></Feature> */}
-      <YoutubeItem
+      {/* <YoutubeItem
         author="Việt Hà 1"
         title="hehhehe"
         avatar="https://images.unsplash.com/photo-1771365095302-5ea5ed3e1388?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D"
@@ -21,26 +30,23 @@ function App() {
         author="Việt Hà 2"
         avatar="https://images.unsplash.com/photo-1770752609290-264c53dc54a3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjF8fHxlbnwwfHx8fHw%3D"
         image="https://images.unsplash.com/photo-1771347819259-487a944f006f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fHw%3D"
-      ></YoutubeItem>
+      ></YoutubeItem> */}
     </div>
   );
 }
 function YoutubeItem(props) {
   return (
     <div className="youtube-item">
-      <div
-        className="youtube-image"
-        //  style={{ height: "250px" }}
-      >
+      <div className="youtube-image" style={{ height: "250px" }}>
         <img
           src={props.image}
-          // style={{
-          //   width: "100%",
-          //   maxWidth: "100%",
-          //   display: "block",
-          //   objectFit: "cover",
-          //   height: "100%",
-          // }}
+          style={{
+            width: "100%",
+            maxWidth: "100%",
+            display: "block",
+            objectFit: "cover",
+            height: "100%",
+          }}
         ></img>
       </div>
       <div className="youtube-footer">
--- src/YoutubeData.jsx
+++ src/YoutubeData.jsx
@@ -13,9 +13,9 @@ export const YoutubeData = [
     title: " mai viet ha",
     author: "author name",
     image:
-      "https://images.unsplash.com/photo-1771365095302-5ea5ed3e1388?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1770752609290-264c53dc54a3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjF8fHxlbnwwfHx8fHw%3D",
     avatar:
-      "https://images.unsplash.com/photo-1771387925581-cb0e1d64f506?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fHw%3D",
+      "https://images.unsplash.com/photo-1771347819259-487a944f006f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fHw%3D",
   },
   {
     id: 3,
