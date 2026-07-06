6f4d145662d73fa07a6ad2777d26bcca6a10d432
maivietha1203 <vhdzvcl@gmail.com>
2026-07-06 14:09:59 +0700

    Bài 13 - Rendering list phần 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import { YoutubeData } from "./YoutubeData";
 // import Feature from "./component/feature";
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5];
+  const double = numbers.map((item) => item * 2);
+  console.log(double);
   return (
     <div className="container">
       {/* <Feature></Feature>
---
+++ src/YoutubeData.jsx
@@ -0,0 +1,29 @@
+export const YoutubeData = [
+  {
+    id: 1,
+    title: " mai viet ha",
+    author: "author name",
+    image:
+      "https://images.unsplash.com/photo-1771365095302-5ea5ed3e1388?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1771387925581-cb0e1d64f506?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fHw%3D",
+  },
+  {
+    id: 2,
+    title: " mai viet ha",
+    author: "author name",
+    image:
+      "https://images.unsplash.com/photo-1771365095302-5ea5ed3e1388?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1771387925581-cb0e1d64f506?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fHw%3D",
+  },
+  {
+    id: 3,
+    title: " mai viet ha",
+    author: "author name",
+    image:
+      "https://images.unsplash.com/photo-1771365095302-5ea5ed3e1388?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1771387925581-cb0e1d64f506?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fHw%3D",
+  },
+];
