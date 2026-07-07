718a250fe8303ad6d95729a497bd71f663720802
maivietha1203 <vhdzvcl@gmail.com>
2026-07-07 14:45:29 +0700

    Bài 29 - Thực hành tictoctoe phần 2

--- src/component/tictactoe/Board.jsx
+++ src/component/tictactoe/Board.jsx
@@ -1,6 +1,9 @@
+import { calculatewinner } from "../../helper";
 import Cell from "./Cell";
 
 const Board = () => {
+  const cells = [null, null, null, "X", "X", "X", null, null, null];
+  console.log(calculatewinner(cells));
   return (
     <div className="game-board">
       {Array(9)
