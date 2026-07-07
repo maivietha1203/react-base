4798f40555bef213346c658294b8be0ff70cda62
maivietha1203 <vhdzvcl@gmail.com>
2026-07-07 09:58:39 +0700

    Bài 25 - Tìm hiểu useState cơ bản phần 4

--- src/component/state/Toggle.jsx
+++ src/component/state/Toggle.jsx
@@ -12,20 +12,23 @@ function Toggle() {
   //   console.log(array);
   const [on, setOn] = useState(false);
   console.log(on);
+  const handleToggle = () => {
+    setOn((on) => !on);
+  };
 
   return (
     <div>
-      <div className={`toggle ${on ? "active" : ""}`}>
+      <div className={`toggle ${on ? "active" : ""}`} onClick={handleToggle}>
         <div className={`spinner ${on ? "active" : ""}`}></div>
       </div>
-      <div className="toggle-control">
+      {/* <div className="toggle-control">
         <div className="toggle-on" onClick={() => setOn(true)}>
           On
         </div>
         <div className="toggle-off" onClick={() => setOn(false)}>
           Off
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
