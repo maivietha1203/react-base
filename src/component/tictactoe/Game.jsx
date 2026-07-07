438aec335fda0c102835985313258e169c7d9f30
maivietha1203 <vhdzvcl@gmail.com>
2026-07-07 15:30:46 +0700

    Bài 31 - Thực hành tictoctoe phần 4

--- src/component/tictactoe/Game.jsx
+++ src/component/tictactoe/Game.jsx
@@ -8,10 +8,22 @@ const Game = () => {
   const [board, setBoard] = useState(Array(9).fill());
   const [xIsNext, setXIsnext] = useState(true);
   const winner = calculateWinner(board);
-  const handleClick = () => {};
+  const handleClick = (index) => {
+    const boardCopy = [...board];
+    if (winner || boardCopy[index]) return;
+    boardCopy[index] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXIsnext((xIsNext) => !xIsNext);
+  };
+  const handleResetGame = () => {
+    setBoard(Array(9).fill());
+    setXIsnext(true);
+  };
   return (
     <div>
       <Board cells={board} onClick={handleClick}></Board>
+      <button onClick={handleResetGame}>Reset Game</button>
+      {/* {winner ? `winner is ${xIsNext ? "X" : "O"}` : ""} */}
     </div>
   );
 };
