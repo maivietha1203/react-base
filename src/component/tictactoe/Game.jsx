4a564c48801a24a5ce792b515d4977a994a84e80
maivietha1203 <vhdzvcl@gmail.com>
2026-07-13 09:10:41 +0700

    Bài 34 - Tìm hiểu useReducer phần 1

--- src/component/tictactoe/Game.jsx
+++ src/component/tictactoe/Game.jsx
@@ -1,37 +1,60 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Board from "./Board";
 
 import "./gameStyles.css";
 import { calculateWinner } from "../../helper";
 
+const initialState = {
+  board: Array(9).fill(),
+  xIsNext: true,
+};
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case "CLICK":
+      console.log("123");
+      break;
+
+    default:
+      break;
+  }
+
+  return state;
+};
 const Game = () => {
   //   const [board, setBoard] = useState(Array(9).fill());
   //   const [xIsNext, setXIsnext] = useState(true);
-  const [state, setState] = useState({
-    board: Array(9).fill(),
-    xIsNext: true,
-  });
+  const [state, dispatch] = useReducer(gameReducer, initialState);
+  // const [state, setState] = useState({
+  //   board: Array(9).fill(),
+  //   xIsNext: true,
+  // });
   const winner = calculateWinner(state.board);
   const handleClick = (index) => {
     const boardCopy = [...state.board];
     if (winner || boardCopy[index]) return;
-    boardCopy[index] = state.xIsNext ? "X" : "O";
+    dispatch({
+      type: "CLICK",
+      payload: {
+        index,
+      },
+    });
+    // boardCopy[index] = state.xIsNext ? "X" : "O";
     // setBoard(boardCopy);
     // setXIsnext((xIsNext) => !xIsNext);
-    setState({
-      ...state,
-      board: boardCopy,
-      xIsNext: !state.xIsNext,
-    });
+    // setState({
+    //   ...state,
+    //   board: boardCopy,
+    //   xIsNext: !state.xIsNext,
+    // });
   };
   const handleResetGame = () => {
     // setBoard(Array(9).fill());
     // setXIsnext(true);
-    setState({
-      ...state,
-      board: Array(9).fill(),
-      xIsNext: true,
-    });
+    // setState({
+    //   ...state,
+    //   board: Array(9).fill(),
+    //   xIsNext: true,
+    // });
   };
   return (
     <div>
